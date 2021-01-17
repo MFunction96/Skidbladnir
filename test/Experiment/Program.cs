85e3f1207fb417e5e2f87411a3a80d35ec2e02ed
MFunction <mfunction96@gmail.com>
2021-01-17 13:16:53 +0800

    Completed basic Checksum implement

---
+++ test/Experiment/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Skidbladnir.Experiment
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            return Task.CompletedTask;
+        }
+    }
+}
