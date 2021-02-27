2910c614c91977e210e05ca68e338ec5055ebc9d
MFunction <MFunction96@users.noreply.github.com>
2021-02-27 23:49:20 +0800

    Completed Checksum function (#5)
    * Completed basic Checksum implement
    
    * Completed Checksum via SHA256
    
    * Completed Hash computing algorithm migration
    
    * Completed full implement on Checksum

--- test/Experiment/Program.cs
+++ test/Experiment/Program.cs
@@ -1,7 +1,10 @@
-﻿using System;
+﻿using Skidbladnir.Common.File;
+using System;
+using System.Collections.Concurrent;
+using System.Diagnostics;
+using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
-using Skidbladnir.Common.File;
 
 namespace Skidbladnir.Experiment
 {
@@ -9,10 +12,16 @@ namespace Skidbladnir.Experiment
     {
         private static readonly CancellationTokenSource SCts = new();
 
+        private static readonly ConcurrentQueue<long> ConcurrentQueue = new();
+
+        private static object _statisticsThreadStatus = new();
+
+        private static FileInfo fileInfo = new(@"E:\Captura\2021-01-01-20-01-58.mp4");
+
         private static async Task Main(string[] args)
         {
             Console.WriteLine("Application started.");
-            Console.WriteLine("Press the ENTER key to cancel...\n");
+            Console.WriteLine("Press the ENTER key to cancel...");
 
             var cancelTask = Task.Run(() =>
             {
@@ -21,15 +30,70 @@ namespace Skidbladnir.Experiment
                     Console.WriteLine("Press the ENTER key to cancel...");
                 }
 
-                Console.WriteLine("\nENTER key pressed: cancelling calculating file.\n");
+                Console.WriteLine("ENTER key pressed: cancelling calculating file.");
                 SCts.Cancel();
             });
+            var watch = new Stopwatch();
 
-            var sumPageSizesTask = Checksum.GetSHA256Async(@"E:\Captura\2021-01-01-20-01-58.mp4");
+            var checksum = new Checksum(SHAAlgorithm.SHA512);
+            checksum.CalculatePositionEvent += ChecksumOnCalculatePositionEvent;
+            checksum.FileInfoEvent += ChecksumFileInfoEvent;
+
+            var statistics = new Thread(StatisticsThread);
+            _statisticsThreadStatus = true;
+            statistics.Start();
+            watch.Start();
+
+            var sumPageSizesTask = checksum.GetFileHashAsync(@"E:\Captura\2021-01-01-20-01-58.mp4", SHAFormatting.Base64, 1 << 20, SCts.Token);
 
             await Task.WhenAny(new[] { cancelTask, sumPageSizesTask });
+            watch.Stop();
+
+            lock (_statisticsThreadStatus)
+            {
+                _statisticsThreadStatus = false;
+            }
+            if (sumPageSizesTask.IsCompletedSuccessfully)
+                Console.WriteLine($"File Hash is {sumPageSizesTask.Result} in {watch.ElapsedMilliseconds}ms.");
+            else if (cancelTask.IsCompletedSuccessfully) Console.WriteLine("Task Cancelled!");
+            else if (sumPageSizesTask.Exception != null) throw sumPageSizesTask.Exception;
+            else throw new SystemException("Fatal error!");
 
             Console.WriteLine("Application ending.");
+        }
+
+        private static void StatisticsThread()
+        {
+            while ((bool) _statisticsThreadStatus)
+            {
+                FileInfo file;
+                lock (fileInfo)
+                {
+                    file = fileInfo;
+                }
+                var peek = ConcurrentQueue.TryDequeue(out var start);
+                if (peek)
+                {
+                    var end = start;
+                    while (ConcurrentQueue.TryDequeue(out var temp)) end = temp;
+                    Console.WriteLine($"Speed: {end - start >> 20} MB/s. {file.Length - end >> 20} MB Remain.");
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void ChecksumFileInfoEvent(object sender, ChecksumEventArg e)
+        {
+            lock (fileInfo)
+            {
+                fileInfo = e.FileInfo;
+            }
+        }
+
+        private static void ChecksumOnCalculatePositionEvent(object sender, ChecksumEventArg e)
+        {
+            ConcurrentQueue.Enqueue(e.Position);
+            //Console.WriteLine($"Position : {e.Position}. Total : {e.Count}");
         }
     }
 }
