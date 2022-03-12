09494803ed9495eb3bb99a577f35c5e96bc0bdc5
MFunction96 <mfunction96@outlook.com>
2022-03-12 09:15:58 +0800

    Initial Mirror Pipeline

---
+++ .azurepipelines/scripts/mirror.ps1
@@ -0,0 +1,23 @@
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.2
+
+param(
+    [Parameter(Mandatory)]
+    [string] $DesUri,
+    [Parameter(Mandatory)]
+    [string] $Branch,
+    [Parameter(Mandatory)]
+    [string] $SrcPAT,
+    [Parameter(Mandatory)]
+    [string] $DesPAT,
+    [bool] $Force
+)
+
+Write-Host $DesUri
+
+Write-Host $Branch
+
+Write-Host $SrcPAT
+
+Write-Host $DesPAT
+
+Write-Host $Force
