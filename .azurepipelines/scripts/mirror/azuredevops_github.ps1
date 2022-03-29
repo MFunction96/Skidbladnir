be5c460a40792abb2f0a561df22c5727dcd92488
MFunction <mfunction96@gmail.com>
2022-03-29 15:49:30 +0800

    Mirror Exp #1

---
+++ .azurepipelines/scripts/mirror/azuredevops_github.ps1
@@ -0,0 +1,37 @@
+# Mirror Azure DevOps Repo to Github
+# Reference
+# https://docs.microsoft.com/en-us/powershell/module/microsoft.powershell.core/about/about_functions_advanced_parameters?view=powershell-7.2
+# https://github.com/xware-gmbh/azure-devops-to-github-mirror/blob/main/git_mirror.sh
+
+param(
+    [Parameter(Mandatory)]
+    [string] $Organization,
+    [Parameter(Mandatory)]
+    [string] $Project,
+    [Parameter(Mandatory)]
+    [string] $Repository,
+    [Parameter(Mandatory)]
+    [string] $DesRepo,
+    [Parameter(Mandatory)]
+    [string] $Branch,
+    [Parameter()]
+    [switch] $Force
+)
+
+
+$SrcUri = "https://$env:SRC_PAT@dev.azure.com/$Organization/$Project/_git/$Repository"
+Write-Host $SrcUri
+
+$DesUri = "https://$env:DES_PAT@github.com/$DesRepo"
+Write-Host $DesUri
+
+$TmpDir = "TMP_$Repository"
+git clone $SrcUri $TmpDir --single-branch --branch $Branch
+
+Set-Location $TmpDir
+
+git push $DesUri --force
+
+Set-Location ..
+
+Remove-Item -Path $TmpDir -Recurse -Force
