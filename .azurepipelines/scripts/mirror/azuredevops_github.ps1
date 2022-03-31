69c75822e022398bafbdf9be19658f69008a1d38
MFunction <mfunction96@gmail.com>
2022-03-31 08:56:23 +0800

    Mirror Exp #6

--- .azurepipelines/scripts/mirror/azuredevops_github.ps1
+++ .azurepipelines/scripts/mirror/azuredevops_github.ps1
@@ -18,23 +18,21 @@ param(
     [switch] $Force
 )
 
-Write-Host $env:TMP
-Write-Host $env:TEMP
-
 $SrcUri = "https://$env:SRC_PAT@dev.azure.com/$Organization/$Project/_git/$Repository"
 $DesUri = "https://$env:DES_PAT@github.com/$DesRepo"
-$TmpDir = "TMP_$Repository"
+$TmpDir = "$env:TMP/TMP_$Repository"
+$WorkFolder = "$PSScriptRoot/../../.."
 
-try {
-    Set-Location $env:TMP
-    git clone $SrcUri $TmpDir --single-branch --branch $Branch
+try {    
+    git clone $SrcUri $TmpDir --single-branch --branch $Branch --quiet
     Set-Location $TmpDir
     git push $DesUri --force
-    Set-Location $env:TMP
+    break
 }
 catch {
-    
+    continue
 }
 finally {
+    Set-Location $WorkFolder
     Remove-Item -Path $TmpDir -Recurse -Force
 }
