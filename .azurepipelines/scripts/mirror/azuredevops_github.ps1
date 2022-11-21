864904d3b155107a9062eda5efa9bd8d89015e5f
MFunction96 <mfunction96@outlook.com>
2022-11-21 17:07:43 +0800

    Update Mirror Pipeline

--- .azurepipelines/scripts/mirror/azuredevops_github.ps1
+++ .azurepipelines/scripts/mirror/azuredevops_github.ps1
@@ -11,31 +11,66 @@ param(
     [Parameter(Mandatory)]
     [string] $Repository,
     [Parameter(Mandatory)]
-    [string] $DesRepo,
+    [string] $DesRepoUser,
     [Parameter(Mandatory)]
     [string] $Branch,
     [Parameter()]
     [switch] $Force
 )
 
-$SrcUri = "https://$env:SRC_PAT@dev.azure.com/$Organization/$Project/_git/$Repository"
-$DesUri = "https://$env:DES_PAT@github.com/$DesRepo"
+if ($IsLinux -and [string]::IsNullOrEmpty($env:TMP))
+{
+    $env:TMP = "/tmp"
+}
+
+$Base64PAT = [System.Convert]::ToBase64String([System.Text.Encoding]::UTF8.GetBytes("`:$env:SRCPAT"))
+$SrcUri = "https://dev.azure.com/$Organization/$Project/_git/$Repository"
+$DesUri = "https://$DesRepoUser`:$env:DES_PAT@github.com/$DesRepoUser/$Repository.git"
 $TmpDir = "$env:TMP/TMP_$Repository"
 $WorkFolder = "$PSScriptRoot/../../.."
 
-try {
-    Start-Process -FilePath "git" -ArgumentList "clone $SrcUri $TmpDir --single-branch --branch $Branch" -Wait -NoNewWindow
-    Set-Location $TmpDir
-    $pushArgs = "push $DesUri"
-    if ($Force) {
-        $pushArgs = "$pushArgs --force"
+if ([System.IO.Directory]::Exists($TmpDir))
+{
+    [System.IO.Directory]::Delete($TmpDir, $true)
+}
+
+$flag = $true
+for ($i = 0; $i -lt 100; ++$i)
+{
+    $Process = Start-Process -FilePath "git" -ArgumentList "-c http.extraHeader=""Authorization: Basic $Base64PAT"" clone $SrcUri $TmpDir --single-branch --branch $Branch" -Wait -NoNewWindow -PassThru
+    if ($Process.ExitCode -eq 0) {	
+        $flag = $false
+        break
     }
-    Start-Process -FilePath "git" -ArgumentList $pushArgs -Wait -NoNewWindow
 }
-catch {
-    
+
+if ($flag)
+{
+    Write-Error "Unable to clone the repository!"
+    Exit 1
 }
-finally {
-    Set-Location $WorkFolder
-    Remove-Item -Path $TmpDir -Recurse -Force
+
+Set-Location $TmpDir
+$pushArgs = "push $DesUri"
+if ($Force) {
+    $pushArgs = "$pushArgs --force"
 }
+
+$flag = $true
+for ($i = 0; $i -lt 100; ++$i)
+{
+    $Process = Start-Process -FilePath "git" -ArgumentList $pushArgs -Wait -NoNewWindow -PassThru
+    if ($Process.ExitCode -eq 0) {	
+        $flag = $false
+        break
+    }
+}
+
+if ($flag)
+{
+    Write-Error "Unable to push the repository!"
+    Exit 1
+}
+
+Set-Location $WorkFolder
+Remove-Item -Path $TmpDir -Recurse -Force
